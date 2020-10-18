f384552a82a810d4494ff7cfa71ecb16f8a59509
Lucas <lucass.lv.lv@gmail.com>
2020-10-18 16:16:27 -0300

    gerador de carga

--- GeradorArquivo.py
+++ GeradorArquivo.py
@@ -2,7 +2,12 @@ import os
 def arquivo(tam):
     arquivo = open("archive.txt", "a")
     file = os.path.getsize ('archive.txt')
-    while(file <= tam):
-        arquivo = open("archive.txt", "a")
-        arquivo.write("a\n")
-        file = os.path.getsize ('archive.txt')
+    while(True):
+        if (file+8 <= tam):
+            arquivo = open("archive.txt", "a")
+            arquivo.write("a\n")
+            file = os.path.getsize ('archive.txt')
+        else:
+            break
+
+arquivo(100)
