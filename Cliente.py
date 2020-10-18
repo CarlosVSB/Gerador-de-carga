6683d789df2c80232b400320f5dad0c615157c07
CarlosVSB <carlosvictor-87@hotmail.com>
2020-10-18 15:03:13 -0300

    primeira adição

---
+++ Cliente.py
@@ -0,0 +1,13 @@
+import socket
+ip = input('digite o ip de conexao: ')
+port = 7000
+addr = ((ip,port)) #define a tupla de endereco
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que eh TCP/IP
+client_socket.connect(addr) #realiza a conexao
+while(True):
+    mensagem = input('digite uma mensagem para enviar ao servidor: ')
+    client_socket.send(mensagem.encode()) #envia mensagem
+    print(client_socket.recv(1024).decode())
+    print ('mensagem enviada')
+
+client_socket.close() #fecha conexao
