a42b70f3bede24a0a1684c88e9ac30f69587a9cc
CarlosVSB <carlosvictor-87@hotmail.com>
2020-10-18 15:42:28 -0300

    mecanismo cliente servidor completo

--- Cliente.py
+++ Cliente.py
@@ -1,13 +1,37 @@
 import socket
-ip = input('digite o ip de conexao: ')
-port = 7000
-addr = ((ip,port)) #define a tupla de endereco
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que eh TCP/IP
-client_socket.connect(addr) #realiza a conexao
-while(True):
-    mensagem = input('digite uma mensagem para enviar ao servidor: ')
-    client_socket.send(mensagem.encode()) #envia mensagem
-    print(client_socket.recv(1024).decode())
-    print ('mensagem enviada')
+from threading import Thread
+import time
 
-client_socket.close() #fecha conexao
+
+# ip = input('digite o ip de conexao: ')
+# print(type(ip))
+# port = 7000
+
+
+class Envio(Thread):
+    def __init__(self,adress,port):
+        Thread.__init__(self)
+        self.adress=adress
+        self.port=port
+    
+    def run(self):
+        addr = ((self.adress,self.port)) #define a tupla de endereco
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF_INET parametro para informar a familia do protocolo, SOCK_STREAM indica que eh TCP/IP
+        client_socket.connect(addr) #realiza a conexao
+        mensagem = 'mensagem'
+        ini=time.time()
+        print(ini)
+        client_socket.send(mensagem.encode()) #envia mensagem
+        print(client_socket.recv(1024).decode())
+        fim=time.time()
+        print(fim)
+        print ('RTT:',fim-ini)
+        client_socket.close() #fecha conexao
+
+cont=0
+
+while cont < 3:
+    obj=Envio("192.168.18.22",7000)
+    obj.start()
+    time.sleep(1)
+    cont+=1
--- Servidor.py
+++ Servidor.py
@@ -1,18 +1,33 @@
 import socket
+from threading import Thread
+import time
+
+
+class bd(Thread):
+    def __init__(self,CSocket,Adress):
+        Thread.__init__(self)
+        self.Sock=CSocket
+        self.Ad=Adress
+        print('Iniciada conexão com cliente',Adress)
+
+    def run(self):
+        recebe = self.Sock.recv(1024)
+        time.sleep(2)
+        self.Sock.send("daijobu".encode())
+
 host = ''
 port = 7000
 addr = (host, port)
-serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket
-serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reiniciliza o socket
-serv_socket.bind(addr) #define a porta e quais ips podem se conectar com o servidor
-serv_socket.listen(10) #define o limite de conexoes
-print ('aguardando conexao')
-con, cliente = serv_socket.accept()  # servidor aguardando conexao
-print('conectado')
-print('aguardando mensagem')
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria o socket
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reiniciliza o socket
+server.bind(addr) #define a porta e quais ips podem se conectar com o servidor
+cont=0
+while cont<3:
+    server.listen(1)
+    print ('aguardando conexao')
+    clientsock, clientAddress = server.accept()
+    print('conectado')
+    newthread = bd(clientsock,clientAddress)
+    newthread.start()
 
-while(True):
-    recebe = con.recv(1024) #define que os pacotes recebidos sao de ate 1024 bytes
-    con.send('teste'.encode())
-    print ('mensagem recebida: '+ recebe.decode())
-serv_socket.close()
+server.close()
