8456e4383e557c488e69fd47f7e3d6efc88b3e02
CarlosVSB <carlosvictor-87@hotmail.com>
2020-10-19 17:10:53 -0300

    sqlitedb

---
+++ Servidor_sqlite.py
@@ -0,0 +1,60 @@
+import socket
+from threading import Thread
+import time
+import sqlite3 as sqlite
+import pickle
+import os
+
+
+class bd(Thread):
+    def __init__(self, CSocket, Adress):
+        Thread.__init__(self)
+        self.Sock = CSocket
+        self.Ad = Adress
+        print('Iniciada conexão com cliente', Adress)
+
+    def run(self):
+        data = b''
+        while True:
+            recebe = self.Sock.recv(1024)
+            data += recebe
+            try:
+                loads = pickle.loads(data)
+                break
+            except:
+                pass
+
+        loads = pickle.loads(data)
+        query = '''insert into arquivos(arq) values (%s);'''
+        tupla = (loads,)
+        cursor.execute(query, tupla)
+        conexao.commit()
+        self.Sock.send("daijobu".encode())
+
+
+conexao = sqlite.connect('Workload.db')
+cursor = conexao.cursor()
+conexao.execute('''CREATE TABLE IF NOT EXISTS arquivos
+                (
+                arq BLOB NOT NULL
+                );''')
+
+
+host = ''
+port = 7000
+addr = (host, port)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # cria o socket
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)  # reiniciliza o socket
+# define a porta e quais ips podem se conectar com o servidor
+server.bind(addr)
+cont = 0
+os.system('nohup ./memoryLog.sh &')
+while cont < 3:
+    server.listen(1)
+    print('aguardando conexao')
+    clientsock, clientAddress = server.accept()
+    print('conectado')
+    newthread = bd(clientsock, clientAddress)
+    newthread.start()
+
+server.close()
